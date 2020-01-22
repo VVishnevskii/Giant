72da68628cb820ba9a859a9a3eae9a6773836875
VVishnevskii <vladislav.vishnevskii@mail.ru>
2020-01-22 18:15:26 +0300

    Настройка кнопки навигации

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// Карточки с ценами
 let blockPrice = document.querySelectorAll('.prices__block');
 let titlePrice = document.querySelectorAll('.prices__price');
 let btnPrice = document.querySelectorAll('.prices__buy-now');
@@ -18,11 +19,17 @@ for (let i = 0; i < blockPrice.length; i++) {
     })
 }
 
-
-let buttonPageHeader = document.querySelector('.main-nav__close-btn');
+// Меню навигации 
+let buttonPageHeader = document.querySelector('.main-nav__btn');
+let buttonClosePageHeader = document.querySelector('.main-nav__close-btn');
 let buttonPageHeaderInner = document.querySelector('.main-nav__list-inner');
 
 buttonPageHeader.addEventListener('click', function() {
-    buttonPageHeader.classList.toggle('main-nav__close-btn--active');
+    buttonPageHeader.classList.toggle('main-nav__btn--active');
     buttonPageHeaderInner.classList.remove('main-nav__list-inner-hidden');
+})
+
+buttonClosePageHeader.addEventListener('click', function() {
+    buttonPageHeaderInner.classList.add('main-nav__list-inner-hidden');
+    buttonPageHeader.classList.toggle('main-nav__btn--active');
 })
